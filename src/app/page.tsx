4ab85baadc0942dbbfe9ede29a1d624613954750
MidVC <lutony040720@gmail.com>
2025-02-17 03:48:33 -0500

    Adjusted font size and avatar size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     style={{backgroundImage: 'url(/backgrounds/snow_mountain1.jpg)'}}>
       <div className='absolute inset-0 bg-black bg-opacity-50'></div>
       <div className='relative z-10 flex flex-col items-center justify-center h-full text-white text-center'>
-        <img src='/avatars/frieren.jpg' alt='MidC Avatar' className='rounded-full mx-auto mb-5 h-60 w-60'></img>
-        <h1 className="text-6xl font-bold">MidC</h1>
+        <img src='/avatars/frieren.jpg' alt='MidC Avatar' className='rounded-full mx-auto mb-5 h-72 w-72'></img>
+        <h1 className="text-7xl font-bold">MidC</h1>
         <div className="flex justify-center mt-5 space-x-5">
               <a href="https://osu.ppy.sh/users/20461077" target="_blank" rel="noopener noreferrer">
                 <img src='/icons/osu.png' alt='osu!' className='rounded-full mx-auto h-20 w-20'></img>
